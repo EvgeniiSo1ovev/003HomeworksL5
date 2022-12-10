5bb03863724b92fc5cd2b381eaf9ee163f8fdad0
Evgenii Solovev <krekersol@mail.ru>
2022-12-10 11:25:35 +0900

    Выполнена первая задача

---
+++ HWL5_1/Program.cs
@@ -0,0 +1,50 @@
+﻿//Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int Prompt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GetArray(int count, int minRange, int maxRange)
+{
+	int[] array = new int[count];
+	for (int i = 0; i < count; i++)
+	{
+		array[i] = new Random().Next(minRange, maxRange+1);
+	}
+	return array;
+}
+
+void PrintArray(int[] array)
+{
+	string mass = $"[{array[0]}";
+	for (int i = 1; i < array.Length; i++)
+	{
+		mass += ", "+Convert.ToString(array[i]);
+	}
+	mass += "]";
+	Console.WriteLine(mass);
+}
+
+int CountOfEven(int[] array)
+{
+	int count = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (array[i] % 2 == 0)
+		{
+			count++;
+		}
+	}
+	return count;
+}
+
+int lengthOfArray = Prompt("Введите количество элементов массива: ");
+int minOfArr = Prompt("Введите минимальный порог случайных значений: ");
+int maxOfArr = Prompt("Введите максимальный порог случайных значений: ");
+int[] array = GetArray(lengthOfArray, minOfArr, maxOfArr);
+PrintArray(array);
+Console.WriteLine($"Количество четных чисел в указанном массиве равно {CountOfEven(array)}");
